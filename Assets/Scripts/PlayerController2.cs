58fa3b4019c433764e38ef144014bd8a619af8eb
Gabrielle Burlison <g.burlison@hotmail.com>
2022-06-07 19:23:35 -0400

    add animation events
    add animation events (footstep sounds) for walking, running, and jumping

--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -144,15 +144,15 @@ public class PlayerController2 : MonoBehaviour
 
     }
 
-    //private void OnFootstep(AnimationEvent animationEvent)
-    //{
-    //    if (animationEvent.animatorClipInfo.weight > 0.5f)
-    //    {
-    //        if (FootstepAudioClips.Length > 0)
-    //        {
-    //            var index = Random.Range(0, FootstepAudioClips.Length);
-    //            AudioSource.PlayClipAtPoint(FootstepAudioClips[index], transform.TransformPoint(controller.center), FootstepAudioVolume);
-    //        }
-    //    }
-    //}
+    private void OnFootstep(AnimationEvent animationEvent)
+    {
+        if (animationEvent.animatorClipInfo.weight > 0.5f)
+        {
+            if (FootstepAudioClips.Length > 0)
+            {
+                var index = Random.Range(0, FootstepAudioClips.Length);
+                AudioSource.PlayClipAtPoint(FootstepAudioClips[index], transform.TransformPoint(controller.center), FootstepAudioVolume);
+            }
+        }
+    }
 }
