dd175e5f8c3190818eb3d606c94cefb37a9f5ebd
ed209uardo <eduardo.rodriguez@gatech.edu>
2022-06-11 15:09:13 -0400

    added GameStatus scriptable object
    Signed-off-by: ed209uardo <eduardo.rodriguez@gatech.edu>

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game Status")]
+
+public class GameStatus : ScriptableObject
+{
+    public float currentHP;
+    public float maxHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
