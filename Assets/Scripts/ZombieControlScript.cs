6f8afd8c296059b2fabb91effea169d02a20831f
Gabrielle Burlison <g.burlison@hotmail.com>
2022-06-29 13:25:17 -0400

    Add Zombie Healthbar
    - added a healthbar prefab that can be applied to enemies and the player
    - zombie takes damage upon pressing Q within a certain distance and damage is reflected on health bar
    - zombie dies upon losing all health

--- Assets/Scripts/ZombieControlScript.cs
+++ Assets/Scripts/ZombieControlScript.cs
@@ -1,48 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ZombieControlScript : MonoBehaviour
 {
     private Animator anim;
     public Transform character;
-    public Transform zombie;
-    //Vector3 move;
+    public GameObject zombie;
     Rigidbody rb;
+    public int maxHealth = 100;
+    public int currentHealth;
+    public GameObject healthBar;
+    public HealthBarScript hbs;
+    public float deathTime = 0f;
 
     // Start is called before the first frame update
     private void Awake() {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+        hbs = healthBar.GetComponent<HealthBarScript>();
     }
     void Start()
     {
         anim.SetFloat("Blend", 0f);
         transform.LookAt(character);
+        currentHealth = maxHealth;
+        hbs.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
+    void Update() {
+        healthBar.transform.position = transform.position + new Vector3(0f,9f,0f);
+        float distance = Vector3.Distance(character.position, transform.position);
+        if (currentHealth == 0 && !anim.GetBool("Dead")) {
+            anim.SetBool("Dead", true);
+            deathTime = Time.time;
+            //Destroy(zombie);
+        }
+        if (anim.GetBool("Dead") && Time.time > deathTime + 6.5) {
+            Destroy(zombie); // destroy after playing death animation
+        } else if (distance < 8) { //fight player
+            if (Input.GetKeyDown(KeyCode.Q)) {
+                TakeDamage(20);
+            }
+        }
+    }
     void FixedUpdate()
     {
-        float distance = Vector3.Distance(character.position, transform.position);
-        print(distance);
-        if (distance < 7) { //fight player
-            transform.LookAt(character);
-            anim.SetBool("Attacking", true);
-        } else if (distance < 30) { // make zombie go towards player fast
-            anim.SetBool("Attacking", false);
-            transform.position = Vector3.MoveTowards(transform.position, character.position, 2.0f * Time.deltaTime);
-            transform.LookAt(character.position);
-            anim.SetFloat("Blend", 1f);
-        } else if (distance < 50) { // make zombie go towards player slowly
-            distance = distance - 30;
-            distance = 1.0f - (distance / 20.0f);
-            transform.position = Vector3.MoveTowards(transform.position, character.position, (distance*2.0f) * Time.deltaTime);
-            transform.LookAt(character.position);
-            anim.SetFloat("Blend", distance);
-        } else { // zombie stand still
-            anim.SetFloat("Blend", 0f);
-            transform.LookAt(character);
+        if (!anim.GetBool("Dead")) {
+            float distance = Vector3.Distance(character.position, transform.position);
+            if (distance < 7) { //fight player
+                //transform.LookAt(character);
+                anim.SetBool("Attacking", true);
+                // if (Input.GetKeyDown(KeyCode.Q)) {
+                //     TakeDamage(20);
+                // }
+            } else if (distance < 30) { // make zombie go towards player fast
+                anim.SetBool("Attacking", false);
+                transform.position = Vector3.MoveTowards(transform.position, character.position, 2.0f * Time.deltaTime);
+                transform.LookAt(character.position);
+                anim.SetFloat("Blend", 1f);
+            } else if (distance < 50) { // make zombie go towards player slowly
+                distance = distance - 30;
+                distance = 1.0f - (distance / 20.0f);
+                transform.position = Vector3.MoveTowards(transform.position, character.position, (distance*2.0f) * Time.deltaTime);
+                transform.LookAt(character.position);
+                anim.SetFloat("Blend", distance);
+            } else { // zombie stand still
+                anim.SetFloat("Blend", 0f);
+                transform.LookAt(character);
+            }
         }
         //if (character) {
             //transform.LookAt(character);
@@ -51,6 +79,15 @@ public class ZombieControlScript : MonoBehaviour
             //transform.Rotate(0, angle, 0);
             //transform.Translate(Vector3.forward*-1*Time.deltaTime);
         //}
+    }
+
+    void TakeDamage(int damage) {
+        if (currentHealth - damage >= 0) {
+            currentHealth -= damage;
+        } else {
+            currentHealth = 0;
+        }
+        hbs.SetHealth(currentHealth);
     }
 
 }
