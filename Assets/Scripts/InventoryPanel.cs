5f4199f6a7ea821856a04eadeb55eed74e28167a
ed209uardo <eduardo.rodriguez@gatech.edu>
2022-07-06 07:40:27 -0400

    inventory further fleshed out

--- Assets/Scripts/InventoryPanel.cs
+++ Assets/Scripts/InventoryPanel.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(CanvasGroup))]
 public class InventoryPanel : MonoBehaviour
 {
-	private CanvasGroup canvasGroup;
+	public Sprite empty;
+	[SerializeField] private GameObject panelSpot;
 	[SerializeField] private GameStatus gameStatus;
+	private List<GameObject> items = new List<GameObject>();
+	private CanvasGroup canvasGroup;
+	private GameObject[] itemRepresentation;
+	private int inventorySpace;
 
-    // Start is called before the first frame update
-    void Awake()
+	// Start is called before the first frame update
+	void Awake()
     {
-        canvasGroup = GetComponent<CanvasGroup>();
+		canvasGroup = GetComponent<CanvasGroup>();
 		gameStatus.inventoryVisible = false;
+
+		inventorySpace = panelSpot.transform.childCount;
+		itemRepresentation = new GameObject[inventorySpace];
+
+		for (int a = 0; a < inventorySpace; a++)
+        {
+			itemRepresentation[a] = panelSpot.transform.GetChild(a).gameObject;
+		}
+
+		ClearInventory();
 	}
 
 	// Update is called once per frame
@@ -31,5 +47,93 @@ public class InventoryPanel : MonoBehaviour
 			canvasGroup.alpha = 0f;
 		}
 
+	}
+
+	public void AddItem(GameObject item)
+    {
+		// need to add check when inventory is full
+		bool itemAdded = false;
+		int a = 0;
+		items.Add(item);
+		objectAsItem equipmentScript = item.GetComponent<objectAsItem>();
+		InventoryItem thisItem = equipmentScript.myItem;
+		while (!itemAdded && a < inventorySpace)
+        {
+			ItemSlot slotScript = itemRepresentation[a].GetComponent<ItemSlot>();
+			if (!slotScript.empty)
+            {
+				if (slotScript.itemID == thisItem.itemID)
+                {
+					itemAdded = true;
+					slotScript.itemCount++;
+					itemRepresentation[a].transform.GetChild(0).GetComponent<Text>().text = slotScript.itemCount + "";
+				}
+            }
+			a++;
+        }
+		if (!itemAdded)
+        {
+			a = 0;
+			while (!itemAdded && a < inventorySpace)
+			{
+				ItemSlot slotScript = itemRepresentation[a].GetComponent<ItemSlot>();
+				if (slotScript.empty)
+                {
+					itemAdded = true;
+					slotScript.itemCount = 1;
+					itemRepresentation[a].transform.GetComponent<Image>().sprite = thisItem.itemPNG;
+					itemRepresentation[a].transform.GetChild(0).GetComponent<Text>().text = slotScript.itemCount + "";
+				}
+				a++;
+			}
+		}
+	}
+
+	public void RemoveItem(GameObject item)
+	{
+		//incomplete
+		items.Remove(item);
+	}
+
+	void ClearInventory()
+	{
+		for (int a = 0; a < inventorySpace; a++)
+		{
+			itemRepresentation[a].transform.GetComponent<Image>().sprite = empty;
+			itemRepresentation[a].transform.GetChild(0).GetComponent<Text>().text = "";
+		}
+	}
+
+	void UpdateInventoryTest()
+	{
+		for (int a = 0; a < items.Count; a++)
+		{
+			objectAsItem equipmentScript = items[a].GetComponent<objectAsItem>();
+			InventoryItem thisItem = equipmentScript.myItem;
+
+			itemRepresentation[a].transform.GetComponent<Image>().sprite = empty;
+			itemRepresentation[a].transform.GetChild(0).GetComponent<Text>().text = "2";
+		}
+	}
+
+	void SecondTest()
+	{
+		objectAsItem tempScript = items[0].GetComponent<objectAsItem>();
+		if (tempScript == null)
+		{
+			Debug.Log("null script");
+		}
+
+		InventoryItem temp2 = tempScript.myItem;
+		if (temp2 == null)
+		{
+			Debug.Log("null scriptable object");
+		}
+
+		empty = tempScript.myItem.itemPNG;
+		if (empty == null)
+        {
+			Debug.Log("null image");
+        }
 	}
 }
--- Assets/Scripts/items/ItemSlot.cs
+++ Assets/Scripts/items/ItemSlot.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class ItemSlot : MonoBehaviour
 {
+    public bool empty;
+    public int itemID;
+    public int itemCount;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        empty = true;
     }
 
     // Update is called once per frame
