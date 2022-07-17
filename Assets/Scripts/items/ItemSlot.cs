2b9a732fc0a94c92e9ae87a5247c33c45b3ffc32
ed209uardo <eduardo.rodriguez@gatech.edu>
2022-07-16 23:33:16 -0400

    added temp description text to inventory

--- Assets/Scripts/items/ItemSlot.cs
+++ Assets/Scripts/items/ItemSlot.cs
@@ -7,16 +7,33 @@ public class ItemSlot : MonoBehaviour
     public bool empty;
     public int itemID;
     public int itemCount;
+    public string itemName;
+    [TextArea(2, 4)]
+    public string itemDescription;
+    //GameObject itemText;
 
     // Start is called before the first frame update
     void Start()
     {
         empty = true;
+        //itemText = this.transform.GetChild(1).gameObject;
+        //itemText.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnMouseOver()
+    {
+        //itemText.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        //itemText.SetActive(false);
     }
 }
